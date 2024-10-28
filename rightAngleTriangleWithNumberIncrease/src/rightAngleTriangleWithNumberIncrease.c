4534f2ef56c4db52499137d741664ff6b53f464d
sravan <yenkeyyes4@gmail.com>
2024-10-28 14:40:28 +0530

    triangle patten with number increasing

---
+++ rightAngleTriangleWithNumberIncrease/src/rightAngleTriangleWithNumberIncrease.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+	int limit,i,j,k=1;
+	printf("enter your limit:");
+	scanf("%d",&limit);
+	for(i=1; i<=limit; i++) {
+		for(j=1; j<=i; j++) {
+			printf("%d ",k++);
+		}
+		printf("\n");
+	}
+}
